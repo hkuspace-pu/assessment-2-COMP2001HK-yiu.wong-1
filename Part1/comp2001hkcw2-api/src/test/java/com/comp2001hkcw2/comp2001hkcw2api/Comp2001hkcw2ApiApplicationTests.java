9a02571ce4f4e0dd7ef580d49d48cf59e982c6bc
benjaminywong <yiu.wong-1@students.plymouth.ac.uk>
2022-02-09 20:47:02 +0800

    initiated api project
    create the api project folders

---
+++ Part1/comp2001hkcw2-api/src/test/java/com/comp2001hkcw2/comp2001hkcw2api/Comp2001hkcw2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp2001hkcw2.comp2001hkcw2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp2001hkcw2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

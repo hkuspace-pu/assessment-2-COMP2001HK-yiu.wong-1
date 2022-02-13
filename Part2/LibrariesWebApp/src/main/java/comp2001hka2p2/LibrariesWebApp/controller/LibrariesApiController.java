f1844089b2a6e578051d09730faee044f4c177c0
benjaminywong <yiu.wong-1@students.plymouth.ac.uk>
2022-02-13 17:56:08 +0800

    created methods
    Created method to get GeoJson from URL and convert to list and return list.
    Created method to convert GeoJson to Json-LD and return Json-LD

---
+++ Part2/LibrariesWebApp/src/main/java/comp2001hka2p2/LibrariesWebApp/controller/LibrariesApiController.java
@@ -0,0 +1,94 @@
+package comp2001hka2p2.LibrariesWebApp.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.geojson.Feature;
+import org.geojson.FeatureCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import comp2001hka2p2.LibrariesWebApp.model.LibrariesGeoJson;
+import comp2001hka2p2.LibrariesWebApp.model.LibrariesJsonLD;
+import ioinformarics.oss.jackson.module.jsonld.JsonldModule;
+
+@RestController
+public class LibrariesApiController 
+{
+	
+	private static final Logger logger = LoggerFactory.getLogger(LibrariesApiController.class);
+			
+	@GetMapping ("/api/geojson/libraries")
+	public String getJson() throws IOException
+	{
+		
+		//Step 1
+		
+//		URL url = new URL("https://battlecat.ddns.net/aaa.json");
+		
+		URL url = new URL("https://storage.googleapis.com/thedataplace-plymouth/resources%2F7ca5c131-ba46-4133-ae6a-0dc8eb8a9281%2F040-02_location-of-libraries-2018.geojson");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		InputStream is = con.getInputStream();
+		ObjectMapper om = new ObjectMapper();
+		FeatureCollection fc = om.readValue(is, FeatureCollection.class);
+		List<Feature> jsonList = fc.getFeatures();
+		List<LibrariesGeoJson> lgjList = new ArrayList<LibrariesGeoJson>();
+		for (int i = 0; i < jsonList.size(); i ++)
+		{
+			Feature f = jsonList.get(i);
+			Map<String, Object> map = f.getProperties();
+			LibrariesGeoJson lgjson = om.convertValue(map, LibrariesGeoJson.class);
+			lgjList.add(lgjson);
+//			logger.info("l[{}]: {}", i, lgjson.getLibraryName());
+			
+//			return lgjList;
+		}
+		
+		//Step 2
+		
+		List<LibrariesJsonLD> ljldList = new ArrayList<LibrariesJsonLD>();
+		for (int i = 0; i < lgjList.size(); i++)
+		{
+			LibrariesGeoJson lgjson = lgjList.get(i);
+			LibrariesJsonLD ljsonLd = new LibrariesJsonLD();
+			ljsonLd.setFid(lgjson.getFid());
+			ljsonLd.setLibraryName(lgjson.getLibraryName());
+			ljsonLd.setAddressLine1(lgjson.getAddressLine1());
+			ljsonLd.setAddressLine2(lgjson.getAddressLine2());
+			ljsonLd.setAddressLine3(lgjson.getAddressLine3());
+			ljsonLd.setPostcode(lgjson.getPostcode());
+			ljsonLd.setLatitude(lgjson.getLatitude());
+			ljsonLd.setLongitude(lgjson.getLongitude());
+			ljsonLd.setWebsite(lgjson.getWebsite());
+			ljldList.add(ljsonLd);
+		}
+		
+		ObjectMapper om2 = new ObjectMapper();
+		JsonldModule jldm = new JsonldModule();
+		om2.registerModule(jldm);
+		String jsonldStr = om2.writeValueAsString(ljldList);
+		
+		return jsonldStr;
+		
+	}
+
+//	private List<LibrariesGeoJson> returnList() throws IOException 
+//	{
+//		
+
+
+		
+//		return resultList;
+//	}
+}
---
+++ Part2/LibrariesWebApp/src/main/java/comp2001hka2p2/LibrariesWebApp/model/LibrariesGeoJson.java
@@ -0,0 +1,107 @@
+package comp2001hka2p2.LibrariesWebApp.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LibrariesGeoJson 
+{
+	@JsonProperty("fid")
+	private String fid;
+	
+	@JsonProperty("LibraryName")
+	private String libraryName;
+	
+	@JsonProperty("AddressLine1")
+	private String addressLine1;
+
+	@JsonProperty("AddressLine2")
+	private String addressLine2;
+
+	@JsonProperty("AddressLine3")
+	private String addressLine3;
+	
+	@JsonProperty("Postcode")
+	private String postcode;
+
+	@JsonProperty("Latitude")
+	private Double latitude;
+
+	@JsonProperty("Longitude")
+	private Double longitude;
+
+	@JsonProperty("Website")
+	private String website;
+
+	public String getFid() {
+		return fid;
+	}
+
+	public void setFid(String fid) {
+		this.fid = fid;
+	}
+
+	public String getLibraryName() {
+		return libraryName;
+	}
+
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getAddressLine3() {
+		return addressLine3;
+	}
+
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	
+}

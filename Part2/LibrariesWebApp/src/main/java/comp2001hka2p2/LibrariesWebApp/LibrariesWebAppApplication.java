c16576a4647273c58ef3ea03274ed86c8e461bba
benjaminywong <yiu.wong-1@students.plymouth.ac.uk>
2022-02-13 13:50:17 +0800

    Rebuilt Part2
    Rebuilt Part2 with spring framework

---
+++ Part2/LibrariesWebApp/src/main/java/comp2001hka2p2/LibrariesWebApp/LibrariesWebAppApplication.java
@@ -0,0 +1,13 @@
+package comp2001hka2p2.LibrariesWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariesWebAppApplication.class, args);
+	}
+
+}
---
+++ Part2/LibrariesWebApp/src/test/java/comp2001hka2p2/LibrariesWebApp/LibrariesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package comp2001hka2p2.LibrariesWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
